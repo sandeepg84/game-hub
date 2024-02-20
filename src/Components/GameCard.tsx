d8ead63db2fef2b7467c695ab1242c2a18a6389e
sandeep <sandeepg84@gmail.com>
2024-02-20 22:00:21 +0530

    Game grid cleanup

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -13,11 +13,11 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformList platforms={game.platforms.map((p) => p.platform)} />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
